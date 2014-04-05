bc9aec5a1ef684051ef99622d6f3d2a27d46b955
eric <tremblay_eric@live.ca>
2014-04-05 11:11:35 -0400

    modif de modal

--- js/controleur.js
+++ js/controleur.js
@@ -23,6 +23,9 @@ $(".close").on('click',function(event){
 
 
 
+//$('#numeroDossier').on('input', function(){
+//console.log('this actually works');
+//});
 
 
 /*
@@ -34,65 +37,59 @@ feedback visuel au moment de la facture
 
 
 //NOT SAFE FOR PRODUCTION 
-var dataTable = "<div id='table1' class='row'>"+
-      "<div class='table-responsive'>"+
-        "<table class='table table-striped'>"+
-          "<thead>"+
-            "<tr>"+
-              "<th>"+
-              "#"+
-              "</th>"+
-              "<th>Item</th>"+
-              "<th>Quantité</th>"+
-              "<th>Prix Unitaire</th>"+
-               "<th>Montant</th>"+
-            "</tr>"+
-          "</thead>"+
-          "<tbody>"+
-            "<tr>"+
-              "<td>1</td>"+
-              "<td>item 1</td>"+
-              "<td>5</td>"+
-              "<td><input id='item1Prix' type='text' name='item1Prix' value='10.00' placeholder='0.0'></td>"+
-              "<td><input id='montant1' type='text' name='montant1' value='50.00' placeholder='0.0' disabled='true'></td>"+
-            "</tr>"+
-            "<tr>"+
-              "<td>2</td>"+
-              "<td>item 2</td>"+
-              "<td>5</td>"+
-              "<td><input id='item2Prix' type='text' name='item2Prix' value='10.00' placeholder='0.0'></td>"+
-              "<td><input id='montant2' type='text'  name='montant2' value='50.00' placeholder='0.0' disabled='true'></td>"+
-            "</tr>"+
-            "<tr>"+
-              "<td>3</td>"+
-              "<td>item 3</td>"+
-              "<td>5</td>"+
-              "<td><input id='item3Prix' type='text' name='item3Prix' value='10.00' placeholder='0.0'></td>"+
-              "<td><input id='montant3' type='text' name='montant3' value='50.00' placeholder='0.0' disabled='true'></td>"+
-            "</tr>"+
-          "</tbody>"+
-          "<tfoot>"+
-            "<tr>"+
-              "<td colspan='3'></td>"+
-              "<td >TPS</td>"+
-              "<td ><input id='montant3' type='text' name='montant3' value='5.00' placeholder='0.0' disabled='true'></td>"+
-            "</tr>"+
-            "<tr>"+
-            "<td colspan='3'></td>"+
-              "<td>TVQ</td>"+
-              "<td ><input id='montant3' type='text' name='montant3' value='2.50' placeholder='0.0' disabled='true'></td>"+
-            "</tr>"+
-            "<tr>"+
-            "<td colspan='3'></td>"+
-              "<td>Sous-Total</td>"+
-              "<td><input id='montant3' type='text' name='montant3' value='7.50' placeholder='0.0' disabled='true'></td>"+
-            "</tr>"+
-            "<tr>"+
-            "<td colspan='3'></td>"+
-              "<td >Total</td>"+
-              "<td ><input id='montant3' type='text' name='montant3' value='10000.00' placeholder='0.0' disabled='true'></td>"+
-            "</tr>"+
-          "</tfoot>"+
-        "</table>"+
-      "</div>"+
-      "</div>";
+//var dataTable = 
+  
+  
+  
+//  <div class="row">
+//                                <table class="table table-hover table-condensed">
+//                                    <thead>
+//                                        <tr>
+//                                            <th>Date</th>
+//                                            <th>Nom du Produit</th>
+//                                            <th>Prix Unitaire</th>
+//                                            <th>Quantite</th>
+//                                            <th>Montant</th>
+//                                        </tr>
+//                                    </thead>
+//                                    <tbody>
+//                                        <tr class="info">
+//                                            <th>2014-01-12</th>
+//                                            <th>Le Seigneur des Anneaux</th>
+//                                            <th>50.00</th>
+//                                            <th>1</th>
+//                                            <th>50.00</th>
+//                                        </tr>
+//                                        <tr class="active">
+//                                            <th>2014-01-12</th>
+//                                            <th>Les recettes de Tante Hilda</th>
+//                                            <th>15.00</th>
+//                                            <th>1</th>
+//                                            <th>5.00</th>
+//                                        </tr>
+//                                        <tr class="info">
+//                                            <th>2014-01-12</th>
+//                                            <th>Le Hobbit</th>
+//                                            <th>15.00</th>
+//                                            <th>1</th>
+//                                            <th>15.00</th>
+//                                        </tr>
+//                                        <tr class="active">
+//                                            <th>2014-01-12</th>
+//                                            <th>Coder Proprement</th>
+//                                            <th>10.00</th>
+//                                            <th>3</th>
+//                                            <th>30.00</th>
+//                                        </tr>
+//                                    </tbody>
+//                                    <tfoot>
+//                                        <tr class="active">
+//                                            <th colspan="4" class="text-right">Grand Total</th>
+//                                            <th>100.00</th>
+//                                        </tr>
+//                                    </tfoot>
+//
+//                                </table>
+//
+//
+//                            </div>
