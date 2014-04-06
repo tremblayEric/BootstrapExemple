7fb4889e7ec2df2c43fb6925ab8b2832913302b2
Ricardo Solon <ricky0604@gmail.com>
2014-04-06 18:02:45 -0400

    amelioration de la presentation du tableau

--- js/controleur.js
+++ js/controleur.js
@@ -69,6 +69,7 @@ $(".modifiable").on("change keyup",calculTotal);
 $("#facturer").hide();
 $("#projetFacture").hide();
 $(".inputCredit").hide();
+$(".projetFacture1").hide();
 
 $("#facturer").on('click', function(){
    
@@ -108,6 +109,7 @@ $("#idProjet").on('change keyup',function(event){
     }
    
    $("#projetFacture").find("span").text(valeurTxT);
+   $(".projetFacture1").show();
    $("#projetFacture").show();
 });
 
