6045a982beeadb3c1f9e2c3ef617fa65df678ff3
Ricardo Solon <ricky0604@gmail.com>
2014-04-10 18:02:08 -0400

    mini script simple test

--- tests/test.js
+++ tests/test.js
@@ -1,28 +1,12 @@
 /*
 exemple du cours à éffacer et mettre nos tests 
 */
-casper.test.begin("Vérification du click d'un site de cours avec capture", 1, function(test) {
+casper.test.begin("Test pour TP3", 1, function(test) {
   // Chargement de la page
-  casper.start('http://jberger.org/');
-
-  // Cliquer sur un lien d'un cours
-  casper.then(function(){
-    this.click("a[href='/inf4150']");
-  });
-
-  // Attendre le temps que le widget Twitter soit complètement chargé
-  casper.then(function(){
-    this.wait(2000);
-  });
-
-  // Prendre un screenshot du site
-  casper.then(function(){
-    this.capture("inf4150.png");
-  });
-
-  // Vérifier l'URL
-  casper.run(function(){
-    test.assertEquals(this.getCurrentUrl(), 'http://jberger.org/inf4150/');
+  casper.start('http://localhost:8000/vue.html', function(){
+    test.assertTitle('Dossier Comptable');
+  }).run(function(){
     test.done();
-  });
+  }
+    );
 });
