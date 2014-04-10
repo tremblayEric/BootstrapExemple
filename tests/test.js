3f0b0cb83b0a58eaf04dcbf5e0e4f0777eaa6a6d
Ricardo Solon <ricky0604@gmail.com>
2014-04-10 19:11:45 -0400

    premier test complet chargement de la page

--- tests/test.js
+++ tests/test.js
@@ -1,11 +1,14 @@
 /*
-exemple du cours à éffacer et mettre nos tests 
+Test TP3 
 */
-casper.test.begin("Test pour TP3", 1, function(test) {
+casper.test.begin("Chargement de la page", 1, function(test) {
   // Chargement de la page
   casper.start('http://localhost:8000/vue.html', function(){
     test.assertTitle('Dossier Comptable');
+    test.assertExists('#page');
+    test.assertExists('#numeroDossier');
     test.assertNotVisible('#page');
+    test.assertVisible('#numeroDossier');
   });
 
   casper.run(function(){
