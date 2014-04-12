e90ee81c5f71d79ba1d3e52e6d698b1e259cde46
eric <tremblay_eric@live.ca>
2014-04-12 11:35:38 -0400

    test liens

--- tests/test.js
+++ tests/test.js
@@ -65,6 +65,22 @@ casper.test.begin('Test des Liens de l\'onglet \'A recevoir\' du Dossier 1',5, f
     });
 });
 
+casper.test.begin('Test  ouverture facture',5, function suite(test) {
+    casper.start("../index.html", function() {
+//    	test.assertExists('a[href="#livre"]');
+this.click("#livre a");
+    	this.wait(500, function() {
+	    	test.assertVisible("#popupLivre");
+	    });
+       
+    });
+    
+    casper.run(function() {
+        test.done();
+    });
+});
+
+
 
 
 
