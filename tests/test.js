29592960542bd2802ff9fb63a75bd05599be0c17
eric <tremblay_eric@live.ca>
2014-04-21 21:23:35 -0400

    dernier commit avant archivage

--- tests/test.js
+++ tests/test.js
@@ -5,7 +5,7 @@ Test TP3
 casper.test.begin("Chargement de la page", 1, function(test) {
  
     casper.start('http://localhost:8000/vue.html', function(){
-        test.assertTitle('Dossier Comptable');
+        test.assertTitle('Dossier Comptable','Validation du titre');
     });
 
     casper.run(function(){
@@ -19,8 +19,8 @@ casper.test.begin("Chargement de la page", 1, function(test) {
 casper.test.begin("Validation du selecteur de client", 2, function(test) {
  
     casper.start('http://localhost:8000/vue.html', function(){
-        test.assertExists('#numeroDossier');
-        test.assertVisible('#numeroDossier');
+        test.assertExists('#numeroDossier','Élement \'numeroDossier\' existe');
+        test.assertVisible('#numeroDossier','Élement \'numeroDossier\' est visible');
     });
 
     casper.run(function(){
@@ -32,8 +32,8 @@ casper.test.begin("Validation du selecteur de client", 2, function(test) {
 casper.test.begin("Validation des accordeons", 2, function(test) {
  
     casper.start('http://localhost:8000/vue.html', function(){
-        test.assertExists('#page');
-        test.assertNotVisible('#page');
+        test.assertExists('#page','Élement page existe');
+        test.assertNotVisible('#page','Élement \'page\' n\'est pas visble par défaut');
     });
     casper.run(function(){
         test.done();
@@ -50,18 +50,18 @@ casper.test.begin('remplir le formulaire membres',5, function suite(test) {
         });
         this.click('#memberLabel');
         this.wait(500,function(){
-            test.assertFieldCSS("#numeroDossier", "12345");
+            test.assertFieldCSS("#numeroDossier", "12345",'Le numéro de dossier égale \'1234\'');
             this.capture('test.png');
             this.click('#accordeonDossierUn');
             this.wait(500,function(){
-                test.assertVisible('#page');
+                test.assertVisible('#page','Élement \'page\' est maintenant visible');
                 this.capture('test1a.png');
                 this.click('#aPopupLivre');
                 this.wait(500,function(){
                     this.capture('test1b.png');
                     this.click('#btnNoteCredit');
                     this.wait(500,function(){
-                        test.assertVisible('#noteCredit');
+                        test.assertVisible('#noteCredit','Élement \'noteCredit\' est maintenant visible');
                         this.capture('test1c.png');
                         this.evaluate(function(){
                             document.querySelector('#item1Prix').value = "6";
@@ -72,7 +72,7 @@ casper.test.begin('remplir le formulaire membres',5, function suite(test) {
                             var newTotal = this.evaluate(function(){
                                 return document.querySelector('#total').value;
                             });
-                            test.assertMatch(newTotal,/^172.86/);
+                            test.assertMatch(newTotal,/^172.86/,'Le total égale 172.86');
                             this.evaluate(function(){
                                 document.querySelector('#idProjet').value = "projet10";
                             });
@@ -82,7 +82,7 @@ casper.test.begin('remplir le formulaire membres',5, function suite(test) {
                                 this.wait(900,function(){
                                     this.capture('test1f.png');
                                 });
-                                test.assertMatch('projet10',/^projet10/);
+                                test.assertMatch('projet10',/^projet10/,'Le projet porte maintenant le nom \'projet10\'');
                                 this.wait(2000,function(){
                                     this.capture('test1g.png');
                                 });
@@ -109,8 +109,8 @@ casper.test.begin('remplir le formulaire membres',5, function suite(test) {
 
 casper.test.begin('Test des Liens des onglets du Dossier 1',2, function suite(test) {
     casper.start("http://localhost:8000/vue.html", function() {
-        test.assertExists('a[href="#aRecevoir"]');
-        test.assertExists('a[href="#aPayer"]');
+        test.assertExists('a[href="#aRecevoir"]','Élement \'aRecevoir existe\'');
+        test.assertExists('a[href="#aPayer"]','Élement \'aPayer\' existe');
 
     });
     
@@ -121,11 +121,11 @@ casper.test.begin('Test des Liens des onglets du Dossier 1',2, function suite(te
 
 casper.test.begin('Test des Liens de l\'onglet \'A recevoir\' du Dossier 1',5, function suite(test) {
     casper.start("http://localhost:8000/vue.html", function() {
-        test.assertExists('a[href="#livre"]');
-        test.assertExists('a[href="#abonnement"]');
-        test.assertExists('a[href="#mobilier"]');
-        test.assertExists('a[href="#divers"]');
-        test.assertExists('a[href="#materiaux"]');
+        test.assertExists('a[href="#livre"]','Élement \'livre\' existe');
+        test.assertExists('a[href="#abonnement"]','Élement \'abonnement\' existe');
+        test.assertExists('a[href="#mobilier"]','Élement \'mobilier\' existe');
+        test.assertExists('a[href="#divers"]','Élement \'divers\' existe');
+        test.assertExists('a[href="#materiaux"]','Élement \'materiaux\' existe');
          
     });
     
@@ -146,29 +146,29 @@ casper.test.begin('Test  ouverture de la facture', 35, function suite(test) {
             test.assertVisible("#uneAdresse","L'adresse est visible");
             test.assertExists('a[href="mailto:#"]',"Il existe un courriel");
 
-            test.assertExists("#result","L'entete de la facture existe");
-            test.assertVisible("#result","L'entete de la facutre est visible");
+            test.assertExists("#result","L'entête de la facture existe");
+            test.assertVisible("#result","L'entête de la facutre est visible");
             test.assertVisible("#tableFacture","La table contenant la facture existe ");
             test.assertVisible("#tableFacture","La table conetnant la facture est visible");
             //item1
-            test.assertVisible("#qteItem1","Le champs de quantite  l'item1 est visible");
+            test.assertVisible("#qteItem1","Le champs de quantité  l'item1 est visible");
             test.assertVisible("#item1Prix","Le champs de prix  l'item1 est visible");
             test.assertVisible("#montant1","Le champs du montant  l'item1 est visible"); 
             //require('utils').dump(this.getElementInfo('#montant1'));\\cette ligne est utlie pour connaitre les info d'un element
             test.assertEquals(this.getElementAttribute('#montant1', 'disabled'),"","Le montant de l'item 1 n'est pas modifiable");
-            test.assertNotVisible("#credit1","Le champs de note de credit  l'item1 est non visible");
+            test.assertNotVisible("#credit1","Le champs de note de crédit  l'item1 est non visible");
             //item2
-            test.assertVisible("#qteItem2","Le champs de quantite  l'item2 est visible");
+            test.assertVisible("#qteItem2","Le champs de quantité  l'item2 est visible");
             test.assertVisible("#item2Prix","Le champs de prix  l'item2 est visible");
             test.assertVisible("#montant2","Le champs du montant  l'item2 est visible");
             test.assertEquals(this.getElementAttribute('#montant2', 'disabled'),"","Le montant de l'item 2 n'est pas modifiable");
-            test.assertNotVisible("#credit2","Le champs de note de credit  l'item2 est non visible");
+            test.assertNotVisible("#credit2","Le champs de note de crédit  l'item2 est non visible");
             //item3
-            test.assertVisible("#qteItem3","Le champs de quantite  l'item3 est visible");
+            test.assertVisible("#qteItem3","Le champs de quantité  l'item3 est visible");
             test.assertVisible("#item3Prix","Le champs de prix  l'item3 est visible");
             test.assertVisible("#montant3","Le champs du montant  l'item3 est visible");
             test.assertEquals(this.getElementAttribute('#montant3', 'disabled'),"","Le montant de l'item 3 n'est pas modifiable");
-            test.assertNotVisible("#credit3","Le champs de note de credit  l'item3 est non visible");
+            test.assertNotVisible("#credit3","Le champs de note de crédit  l'item3 est non visible");
 	   
             //taxe et total
             test.assertVisible("#tps","Le champs de la tps est visible");
@@ -182,8 +182,8 @@ casper.test.begin('Test  ouverture de la facture', 35, function suite(test) {
             test.assertVisible("#idProjet","Le champs du projet est visible");
         
             //bouton note de credit
-            test.assertExists('#btnNoteCredit',"Il existe un element  note de credit");
-            test.assertEquals(this.getElementAttribute('#btnNoteCredit', 'type'),"button","L'element btnNoteCredit est un bouton");
+            test.assertExists('#btnNoteCredit',"Il existe un element \'btnNoteCredit\'");
+            test.assertEquals(this.getElementAttribute('#btnNoteCredit', 'type'),"button","L'élement \'btnNoteCredit\' est un bouton");
            this.capture('test3.png');
 
 
